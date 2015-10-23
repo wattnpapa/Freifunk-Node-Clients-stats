1f270ea21200f52157b6c5d04930d52f3e3eb494
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-10-23 18:19:27 +0200

    Images working?

--- class/Node.php
+++ class/Node.php
@@ -38,32 +38,44 @@ class Node
         if(!$this->checkRRDFileExists()){
             $this->createRRDFile();
         }
-
+        $data = Array();
+        $data[] = time();
+        
         //TODO: Memory Usage
         $memoryUsage = $this->getStatistics()->getMemoryUsage() * 100;
+        $data[] = $memoryUsage;
         //TODO: clients
         $clients = $this->getStatistics()->getClients();
+        $data[] = $clients;
         //TODO: rootfs_usage
         $rootfs = $this->getStatistics()->getRootfsUsage() * 100;
+        $data[] = $rootfs;
         //TODO: loadavg
         $loadavg = $this->getStatistics()->getLoadavg();
+        $data[] = $loadavg;
         //TODO: traffic
         $trafficMgmtRxbytes = $this->getStatistics()->getTraffic()->getMgmtRx()->getBytes();
+        $data[] = $trafficMgmtRxbytes;
         $trafficMgmtRxpackets = $this->getStatistics()->getTraffic()->getMgmtRx()->getPackets();
+        $data[] = $trafficMgmtRxpackets;
         $trafficMgmtTxbytes = $this->getStatistics()->getTraffic()->getMgmtTx()->getBytes();
+        $data[] = $trafficMgmtTxbytes;
         $trafficMgmtTxpackets = $this->getStatistics()->getTraffic()->getMgmtTx()->getPackets();
+        $data[] = $trafficMgmtTxpackets;
         $trafficRxbytes = $this->getStatistics()->getTraffic()->getRx()->getBytes();
+        $data[] = $trafficRxbytes;
         $trafficRxpackets = $this->getStatistics()->getTraffic()->getRx()->getPackets();
+        $data[] = $trafficRxpackets;
         $trafficTxbytes = $this->getStatistics()->getTraffic()->getTx()->getBytes();
+        $data[] = $trafficTxbytes;
         $trafficTxpackets = $this->getStatistics()->getTraffic()->getTx()->getPackets();
+        $data[] = $trafficTxpackets;
         $trafficForwardedBytes = $this->getStatistics()->getTraffic()->getForward()->getBytes();
+        $data[] = $trafficForwardedBytes;
         $trafficForwardedPackets = $this->getStatistics()->getTraffic()->getForward()->getPackets();
-        echo "traffic rx packets:";
-        echo $trafficrxpackets;
-        echo "<br>";
-        echo "<br>";
-        echo "<br>";
-        echo "##############################<br>";
+        $data[] = $trafficForwardedPackets;
+        
+        $ret = rrd_update($this->getRRDFileName(), $data);
     }
 
     private function checkRRDFileExists(){
@@ -74,32 +86,62 @@ class Node
         $options = array(
             "--step", "60",            // Use a step-size of 5 minutes
             "--start", "-6 months",     // this rrd started 6 months ago
-            "DS:memoryUsage:ABSOLUTE:600:0:U",
+            "DS:memoryUsage:ABSOLUTE:600:0:100",
             "DS:clients:ABSOLUTE:600:0:U",
-            "DS:rootfsUsage:ABSOLUTE:600:0:U",
+            "DS:rootfsUsage:ABSOLUTE:600:0:100",
             "DS:loadavg:ABSOLUTE:600:0:U",
-            "DS:trafficRx:ABSOLUTE:600:0:U",
-            "DS:trafficMgmtRx:ABSOLUTE:600:0:U",
-            "DS:trafficTx:ABSOLUTE:600:0:U",
-            "DS:trafficMgmtTx:ABSOLUTE:600:0:U",
-            "DS:trafficForwarded:ABSOLUTE:600:0:U",
+            "DS:trafMgmtRxBy:ABSOLUTE:600:0:U",
+            "DS:trafMgmtRxPa:ABSOLUTE:600:0:U",
+            "DS:trafMgmtTxBy:ABSOLUTE:600:0:U",
+            "DS:trafMgmtTxPa:ABSOLUTE:600:0:U",            
+            "DS:trafRxBy:ABSOLUTE:600:0:U",
+            "DS:trafRxPa:ABSOLUTE:600:0:U",            
+            "DS:trafTxBy:ABSOLUTE:600:0:U",
+            "DS:trafTxPa:ABSOLUTE:600:0:U",            
+            "DS:trafForwardBy:ABSOLUTE:600:0:U",
+            "DS:trafForwardPa:ABSOLUTE:600:0:U",
             "RRA:AVERAGE:0.5:1:288",
             "RRA:AVERAGE:0.5:12:168",
             "RRA:AVERAGE:0.5:228:365",
         );
 
         $ret = rrd_create($this->getRRDFileName(), $options);
-
+        echo rrd_error();
     }
 
     public function getRRDFileName(){
         return "rrdData/nodes/".$this->nodeinfo->getNodeId().".rrd";
     }
-
-    private function makeGraph(){
-
+    
+    public function getFileName(){
+        return "graphs/nodes/".$this->nodeinfo->getNodeId().".png";
     }
 
+    private function makeGraph(){
+        echo "make Graph <br>";
+        $this->createGraph("-1h","Clients");
+    }
+    
+    private function createGraph($start, $title) {
+        $options = array(
+            "--slope-mode",
+            "--start", $start,
+            "--title=$title",
+            "--vertical-label=Clients",
+            "--lower=0",
+            "DEF:clients=".$this->getRRDFileName().":clients:AVERAGE",
+            "CDEF:tclients=clients,300,*",
+            "AREA:tclients#00FF00:Successful attempts",
+            "COMMENT:\\n",
+            "GPRINT:tclients:AVERAGE:successful attempts %6.2lf",
+        );
+        echo "ok";
+        $ret = rrd_graph($this->getFileName(),$options);
+        echo rrd_error();
+        echo "<br>";
+    }
+
+     
     /**
      * @return mixed
      */
