28df3a6c13467818abac7b881051f5688e8bc72e
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-07 23:12:28 +0100

    use demo data for testing

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 include("class/Data.php");
 
-$data = new Data("http://mesh.sjr-ol.de/data/nodes.json");
+//$data = new Data("http://mesh.sjr-ol.de/data/nodes.json");
+$data = new Data("demo2.json");
 
 
 
