73d3817538cb62f7d971b242caadc975b7718cf4
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-08 01:37:09 +0100

    Add URL GET

--- getGraph.php
+++ getGraph.php
@@ -2,10 +2,32 @@
 
 include("class/Node.php");
 
-$mac = "54e6fcaf3efc";
+//DEFAULTS
+$mac = "14cc2091ecd0";
+$width = 800;
+$height = 200;
+$type = "traffic";
+
+if(isset($_GET['mac']))
+    $mac = $_GET['mac'];
+
+if(isset($_GET['type']))
+    $type = $_GET['type'];
+
+if(isset($_GET['width']))
+    $width = $_GET['width'];
+
+if(isset($_GET['height']))
+    $height = $_GET['height'];
+
+
 $node = new Node();
 $nodeinfo = new NodeInfo();
 $nodeinfo->setNodeId($mac);
 $node->setNodeinfo($nodeinfo);
-$node->makeGraph(800,200);
+$node->makeGraph($type,$width,$height);
+
+$im = file_get_contents($node->getFileName($type));
+header('content-type: image/png');
+echo $im; 
 ?>
