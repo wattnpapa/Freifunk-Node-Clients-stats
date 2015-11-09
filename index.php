d45947f2f5f685170f4a41608c75f51080e5e34b
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-09 21:16:31 +0100

    stuff

--- index.php
+++ index.php
@@ -14,20 +14,39 @@ if($type == "Node"){
 
 <html>
 <head>
-    <title>FF Stats</title>
+    <title>Freifunk Statsitics</title>
+    <!-- Bootstrap core CSS -->
+    <link href="lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
 </head>
 <body>
-    <?php
+<div class="container">
+    <div class="header clearfix">
+        <!--<nav>
+            <ul class="nav nav-pills pull-right">
+                <li role="presentation" class="active"><a href="#">Home</a></li>
+                <li role="presentation"><a href="#">About</a></li>
+                <li role="presentation"><a href="#">Contact</a></li>
+            </ul>
+        </nav>-->
+        <h3 class="text-muted">Freifunk Statsitics</h3>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+        <?php
         if($type == "System"){
             ?>
             <h1>Last Hour</h1>
-            <img src="getSystemGraph.php?interval=1H" />
+            <img src="getSystemGraph.php?interval=1H&width=800" />
             <h1>Last Day</h1>
-            <img src="getSystemGraph.php?interval=1D" />
+            <img src="getSystemGraph.php?interval=1D&width=800" />
             <h1>Last Month</h1>
-            <img src="getSystemGraph.php?interval=1m" />
+            <img src="getSystemGraph.php?interval=1m&width=800" />
             <h1>Last Year</h1>
-            <img src="getSystemGraph.php?interval=1Y" />
+            <img src="getSystemGraph.php?interval=1Y&width=800" />
             <?php
         }
         if($type == "Node"){
@@ -62,7 +81,13 @@ if($type == "Node"){
             <img src="getNodeGraph.php?interval=1Y&mac=<?php echo $mac;?>&type=loadavg" />
             <?php
         }
-    ?>
+        ?>
+        </div>
+    </div>
+</div> <!-- /container -->
+
+
+
 
 </body>
 </html>
