5d14d2e681316e4372599667dbaad933ab540c53
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-14 14:04:26 +0100

    Add Class rrd for static functions

---
+++ class/RRD.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johannes
+ * Date: 14.11.15
+ * Time: 13:59
+ */
+class RRD
+{
+    public static function getDSFromRRDFile($file){
+        $info = rrd_info($file);
+        $suchmuster = "/(ds\[)(.*[a-zA-Z0-9])(\])/";
+        $ds = array();
+        foreach ($info as $key => $value){
+            preg_match($suchmuster, $key, $treffer);
+            if(count($treffer) > 1){
+                if(!in_array($treffer[2],$ds)){
+                    $ds[] = $treffer[2];
+                }
+            }
+        }
+        return $ds;
+    }
+}
