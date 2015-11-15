ca691005758e1e86d31c78d052b885d3cf7613de
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-15 17:37:19 +0100

    Use bash

---
+++ updatescripts/0_0_3To0_0_4.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+#rename nodes to nodesOnline
+rrdtool tune ../rrdData/system/system.rrd --data-source-rename nodes:nodesOnline
+
+#move file to tmp
+mv ../rrdData/system/system.rrd /tmp/tmp.rrd
+
+#create new rrd with old data
+rrdtool create ../rrdData/system/system.rrd --step 60 DS:clients:GAUGE:600:0:U DS:nodesOnline:GAUGE:600:0:U DS:nodesOffline:GAUGE:600:0:U RRA:AVERAGE:0.5:1:10080 RRA:AVERAGE:0.5:60:8760 RRA:AVERAGE:0.5:1440:5256
