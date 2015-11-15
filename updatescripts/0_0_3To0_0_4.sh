3fcfa366706d14ade3bd4227b1e32145b96f3818
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-15 17:42:47 +0100

    add mising soruce

--- updatescripts/0_0_3To0_0_4.sh
+++ updatescripts/0_0_3To0_0_4.sh
@@ -7,4 +7,4 @@ rrdtool tune ../rrdData/system/system.rrd --data-source-rename nodes:nodesOnline
 mv ../rrdData/system/system.rrd /tmp/tmp.rrd
 
 #create new rrd with old data
-rrdtool create ../rrdData/system/system.rrd --step 60 DS:clients:GAUGE:600:0:U DS:nodesOnline:GAUGE:600:0:U DS:nodesOffline:GAUGE:600:0:U RRA:AVERAGE:0.5:1:10080 RRA:AVERAGE:0.5:60:8760 RRA:AVERAGE:0.5:1440:5256
+rrdtool create ../rrdData/system/system.rrd --step 60 DS:clients:GAUGE:600:0:U DS:nodesOnline:GAUGE:600:0:U DS:nodesOffline:GAUGE:600:0:U RRA:AVERAGE:0.5:1:10080 RRA:AVERAGE:0.5:60:8760 RRA:AVERAGE:0.5:1440:5256 --source /tmp/tmp.rrd
