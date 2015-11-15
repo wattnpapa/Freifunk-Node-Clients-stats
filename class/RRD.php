a4e7f586b2508fd88b51330ca2991776e60dbd99
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-15 11:07:26 +0100

    add a lor for add a new DS to existing RRD File

--- class/RRD.php
+++ class/RRD.php
@@ -23,8 +23,51 @@ class RRD
         return $ds;
     }
 
-    public static function addDS2RRDFile($rrdfile,$dsname,$type,$heartbeat,$min,$max){
-        $option = array("DS:".$dsname.":".$type.":".$heartbeat.":".$min.":".$max);
-        rrd_tune($rrdfile,$option);
+    public static function addDS2RRDFile($rrdFileName,$newDS){
+        //Get Existing Datasources
+        $datassources = self::getDSFromRRDFile($rrdFileName);
+        //Add New DataSources
+        $datassources[] = $newDS;
+
+        //Create array with DS Options
+        $dsoptions = array();
+        foreach($datassources as $ds){
+            $dsoptions[] = RRD::getDSOption($ds);
+        }
+
+        //get RRD Options
+        $rrdOptions = RRD::getRRDFileOptions($dsoptions);
+
+        //Move OriginalFile to /tmp
+        $tmpFileName = "/tmp/tmp.rrd";
+        rename($rrdFileName,$tmpFileName);
+
+        //add source options
+        $rrdOptions[] = "--source";
+        $rrdOptions[] = $tmpFileName;
+
+        //create new RRDFile
+        RRD::createRRDFile($rrdFileName,$rrdOptions);
     }
+
+    public static function createRRDFile($filename,$options){
+        $ret = rrd_create($filename, $options);
+        echo rrd_error();
+    }
+
+    public static function getRRDFileOptions($dataSources){
+        $options = array("--step", "60");
+        foreach($dataSources as $data) {
+            $options[] = $data;
+        }
+        $options[] = "RRA:AVERAGE:0.5:1:10080"; //every minute one week
+        $options[] = "RRA:AVERAGE:0.5:60:8760"; //
+        $options[] = "RRA:AVERAGE:0.5:1440:5256";
+        return $options;
+    }
+
+    public static function getDSOption($dsname){
+        return "DS:".$dsname.":GAUGE:600:0:U";
+    }
+
 }
