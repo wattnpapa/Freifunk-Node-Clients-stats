f3e2c620bf34638ea882e50b4a431a6b780e99a2
Johannes Rudolph <johannes.rudolph@gmx.com>
2015-11-22 14:49:55 +0100

    Add RRDDSMapping

--- class/HardwareRRDDSMapping.php
+++ class/HardwareRRDDSMapping.php
@@ -1,21 +1,21 @@
 <?php
 
 include("RRDDSMapping.php");
-
 /**
  * Created by IntelliJ IDEA.
  * User: johannes
  * Date: 14.11.15
  * Time: 15:22
  */
-class FirmwareRRDDSMapping extends RRDDSMapping
+class HardwareRRDDSMapping extends RRDDSMapping
 {
+
     /**
      * FirmwareRRDDSMapping constructor.
      * @param $mapping
      */
     public function __construct() {
-        $this->mappingFile = dirname(__FILE__)."/../rrdData/system/firmwareMapping.json";
+        $this->mappingFile = dirname(__FILE__)."/../rrdData/system/hardwareMapping.json";
         parent::__construct();
     }
 
---
+++ class/RRDDSMapping.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: johannes
+ * Date: 14.11.15
+ * Time: 15:22
+ */
+abstract class FirmwareRRDDSMapping
+{
+    protected $mapping;
+    protected $mappingFile;
+
+    /**
+     * FirmwareRRDDSMapping constructor.
+     * @param $mapping
+     */
+    public function __construct() {
+        $this->readFromFile();
+    }
+
+    private function readFromFile(){
+        if(!file_exists($this->mappingFile)){
+            $this->mapping = array();
+            $this->wrtieToFile();
+        }
+        $fd = fopen($this->mappingFile, 'r');
+        $this->mapping = json_decode(fread($fd,filesize($this->mappingFile)),true);
+        fclose($fd);
+    }
+
+    private function wrtieToFile(){
+        $fd = fopen($this->mappingFile, 'w');
+        fwrite($fd,json_encode($this->mapping));
+        fclose($fd);
+    }
+
+    public function getNameForCode($code){
+        return $this->mapping[$code];
+    }
+
+    public function getCodeForName($name){
+        $pattern = "/([^a-zA-Z0-9]+)/";
+        $code = preg_replace($pattern,"_",$name);
+        $code = substr($code,0,15);
+        return $code;
+    }
+
+    public function addNameToMapping($name){
+        $code = $this->getCodeForName($name);
+        if(!in_array($name,$this->mapping)){
+            $this->mapping[$code] = $name;
+            $this->wrtieToFile();
+        }
+        return $code;
+    }
+
+
+}
