448e6de6eb0d3f97e18a35ba2a5a29757a3a33be
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-03-25 11:47:57 +0100

    use mesh viewer Config for data urls

--- fillData.php
+++ fillData.php
@@ -3,8 +3,13 @@
 include(dirname(__FILE__) ."/class/Data.php");
 
 $config = json_decode(file_get_contents(dirname(__FILE__)."/config.json"),true);
+$meshviewerConfig = json_decode(file_get_contents($config['meshviewerConfig']));
 
-$url =  $config["dataUrl"];
+$url = array();
+foreach($meshviewerConfig['dataPath'] as $dataPath){
+    $url [] = $dataPath."nodes.json";
+}
+
 $data = new Data($url);
 
 ?>
